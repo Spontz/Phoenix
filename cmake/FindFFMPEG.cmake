3c55d4d1891895b843a989ed1f2bb6cc5973745c
xphere <xpherezn@gmail.com>
2019-06-28 21:44:05 +0200

    FFMPEG build script

--- cmake/FindFFMPEG.cmake
+++ cmake/FindFFMPEG.cmake
@@ -5,5 +5,6 @@
 set(FFMPEG_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/include/ffmpeg)
 include_directories(${FFMPEG_INCLUDE_DIRS})
 
-set(FFMPEG_LIBRARIES avcodec avdevice avfilter avformat avutil postproc swresample swscale)
+#set(FFMPEG_LIBRARIES avcodec avdevice avfilter avformat avutil postproc swresample swscale)
+set(FFMPEG_LIBRARIES avcodec avformat avutil swresample swscale)
 
