21493a70ddc19dba96eafad7fb9a41d41d8e90a4
Isaac Lascasas <isaaclascasas@gmail.com>
2019-08-20 20:43:33 +0200

    ++

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,3 +1,3 @@
 call install_vcpkg_libs.bat
-Powershell.exe -executionpolicy remotesigned -File install_third_party_libs.ps1
+Powershell.exe -executionpolicy remotesigned -File .\install_third_party_libs.ps1
 call run_cmake.bat
