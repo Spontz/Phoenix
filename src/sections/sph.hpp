f3114a544ca0d0fcde9e75604f63dd117babbfa3
Isaac Lascasas <isaaclascasas@gmail.com>
2019-08-28 20:10:59 +0200

    SPH section [WIP].

--- src/sections/sph.hpp
+++ src/sections/sph.hpp
@@ -2,20 +2,35 @@
 
 #include "main.h"
 
+#include <map>
 #include <vector>
+
+// Smoothed Particle Hydrodynamics section
+// A coupled particle system simulator + renderer
+// Lagrangian method of Navier-Stokes eq. integration
+// Trivial acceleration via 3D grid
+// Reference C++ simulator (CS later).
+
+// 1) Simulates fluid interaction
+
+// 2) Renders spheres as (z-aware) projection plane parallel rectangular billboards
+//    (vs,gs,ps)->1 channel float32 rt (view space z)
+
+// 3) Performs screen space operations to smooth and light ~isosurface
+//    WIP: pos + normal reconstruction from z?
 
 namespace sections {
 	class SPH : public Section {
+		// IS units.
 
 	private:
 		typedef float real;
 		typedef glm::vec<3, real> v3;
 
 		struct Particle {
-			v3 p_;
-			v3 v_;
-			v3 a_;
-			v3 f_;
+			v3 v;
+			v3 a;
+			v3 f;
 		};
 
 	private:
@@ -28,10 +43,15 @@ namespace sections {
 
 	private:
 		static constexpr real	kSimFreqHz = 60; // hz
-		static constexpr size_t	kBounds = 100;
+		typedef std::tuple<int, int, int> t_grid_index;
+		typedef std::vector<size_t> t_grid_item;
+		typedef std::map<t_grid_index, t_grid_item> t_grid;
+		t_grid grid_;
 
 	private:
-		std::vector<Particle>	Particles_;
+		std::vector<v3>			particles_pos_; // separated for perfomance
+		std::vector<Particle>	particles_;
+		int pass_sph_billboard_ = -1;
 	};
 
 }
