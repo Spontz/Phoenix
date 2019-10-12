b4f91266a2252984a7547acbe60616ca0409c136
xphere <xpherezn@gmail.com>
2019-10-12 19:58:47 +0200

    WIP^: cameraSection fixing

--- src/sections/cameraSection.cpp
+++ src/sections/cameraSection.cpp
@@ -20,6 +20,10 @@ bool sCamera::load() {
 	local = (camera_section*)malloc(sizeof(camera_section));
 	this->vars = (void *)local;
 
+	// Load parameters
+	local->freeCam = static_cast<int>(this->param[0]);
+
+
 	// Load the camera splines
 	for (int i = 0; i < this->spline.size(); i++) {
 		this->spline[i]->load();
@@ -33,13 +37,25 @@ void sCamera::init() {
 
 void sCamera::exec() {
 	local = (camera_section *)this->vars;
-	ChanVec	new_pos;
-	this->spline[0]->MotionCalcStep(new_pos, this->runTime);
-	
-	DEMO->camera->setCamera(glm::vec3(new_pos[0], new_pos[1], new_pos[2]),
-							glm::vec3(new_pos[3], new_pos[4], new_pos[5]),
-							new_pos[6], new_pos[7], new_pos[8]);
 
+	// TODO: We could load several splines and use one or another spline, depending on one variable,
+	// so, we could be able to change the camera depending on a variable
+
+	// If freeCam is active, we do nothing
+	if (local->freeCam)
+		return;
+
+	if (!this->spline.empty()) {
+		ChanVec	new_pos;
+
+		// Calculate the motion step of the first spline and set it to "new_pos"
+		this->spline[0]->MotionCalcStep(new_pos, this->runTime);
+		
+		DEMO->camera->setCamera(glm::vec3(new_pos[0], new_pos[1], new_pos[2]),
+			glm::vec3(new_pos[3], new_pos[4], new_pos[5]),
+			new_pos[6], new_pos[7], new_pos[8]);
+	}
+	
 	
 }
 
