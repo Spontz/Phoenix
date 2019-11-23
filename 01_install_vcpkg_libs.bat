14ad9cfb98efc6e7453914a2ee0fc5a49ae92dbd
Isaac Lascasas <isaaclascasas@gmail.com>
2019-11-23 18:14:40 +0100

    Fixed assimp not linking.

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -2,6 +2,7 @@ rmdir vcpkg /s /q
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 git pull
+git checkout e99a3f913202bc3da77ccf4b65391e245079eb05
 call bootstrap-vcpkg.bat
 vcpkg install freetype assimp ffmpeg stb glm glad glfw3 exprtk --triplet x64-windows
 cd..
