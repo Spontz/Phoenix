dba31f28e685c7e4bce23d220bc28199ecbeb4e7
Isaac Lascasas <isaaclascasas@gmail.com>
2019-12-02 15:11:49 +0100

    Changed build to x64-windows-static.

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -4,5 +4,5 @@ cd vcpkg
 git pull
 git checkout e99a3f913202bc3da77ccf4b65391e245079eb05
 call bootstrap-vcpkg.bat
-vcpkg install freetype assimp ffmpeg stb glm glad glfw3 exprtk --triplet x64-windows
+vcpkg install freetype assimp ffmpeg stb glm glad glfw3 exprtk --triplet x64-windows-static
 cd..
