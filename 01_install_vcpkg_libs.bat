0bc18979cf050d12d222fcde7f3ab0230254c792
Isaac Lascasas <isaaclascasas@gmail.com>
2020-05-24 04:50:48 +0200

    Fixed Assimp 5 static linkage problems.

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -1,8 +1,7 @@
 rmdir vcpkg /s /q
-git clone https://github.com/microsoft/vcpkg.git
+git clone https://github.com/Spontz/vcpkg.git
 cd vcpkg
 git pull
-git checkout e99a3f913202bc3da77ccf4b65391e245079eb05
 call bootstrap-vcpkg.bat
 vcpkg install freetype assimp ffmpeg stb glm glad glfw3 exprtk --triplet x64-windows-static
 cd..
--- 03_run_cmake.bat
+++ 03_run_cmake.bat
@@ -1,9 +1,3 @@
-rmdir phoenix_vs2017 /s /q
-mkdir phoenix_vs2017
-cd phoenix_vs2017
-cmake.exe -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 15 2017" ..
-cd..
-
 rmdir phoenix_vs2019 /s /q
 mkdir phoenix_vs2019
 cd phoenix_vs2019
