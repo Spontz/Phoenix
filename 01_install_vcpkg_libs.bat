5a64b5cb4766d0a1dd8abfd499f3386ba4afc88e
Isaac Lascasas <isaaclascasas@gmail.com>
2020-09-14 17:37:29 +0200

    Fixes LINK : fatal error LNK1104: cannot open file irrxmld.lib

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -2,7 +2,7 @@ rmdir vcpkg /s /q
 REM git clone https://github.com/Spontz/vcpkg.git
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-git pull
+git checkout bff4dd9c944b9fbad40ec1a36d7c2365287ce0a6
 call bootstrap-vcpkg.bat
 vcpkg install freetype assimp ffmpeg stb glm glad imgui glfw3 exprtk --triplet x64-windows-static
 cd..
