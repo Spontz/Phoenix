8d6a989560b1516b6e0ea5e354dc5140b2145713
Isaac Lascasas <isaaclascasas@gmail.com>
2020-10-12 20:54:19 +0200

    CMake/Vcpkg: build FFmpeg with hardware acceleration support.

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -2,7 +2,7 @@ rmdir vcpkg /s /q
 REM git clone https://github.com/Spontz/vcpkg.git
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-git checkout bff4dd9c944b9fbad40ec1a36d7c2365287ce0a6
+REM git checkout bff4dd9c944b9fbad40ec1a36d7c2365287ce0a6
 call bootstrap-vcpkg.bat
-vcpkg install freetype assimp ffmpeg stb glm glad imgui glfw3 exprtk --triplet x64-windows-static
+vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui glfw3 exprtk --triplet x64-windows-static
 cd..
--- 01_update_vcpkg_libs.bat
+++ 01_update_vcpkg_libs.bat
@@ -1,7 +1,7 @@
 cd vcpkg
 git pull
 .\bootstrap-vcpkg.bat
-.\vcpkg install freetype assimp ffmpeg stb glm glad imgui glfw3 exprtk --triplet x64-windows-static
+.\vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui glfw3 exprtk --triplet x64-windows-static
 .\vcpkg upgrade --no-dry-run
 cd..
 pause
