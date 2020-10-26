3e3b0e5ce0d4ff0ec37eddcd1a82c878cd07ece0
xphere <xpherezn@gmail.com>
2020-10-26 20:14:49 +0100

    Updated drawParticles default shaders

--- data/pool/shaders/drawParticles/drawParticles.glsl
+++ data/pool/shaders/drawParticles/drawParticles.glsl
@@ -20,10 +20,11 @@ void main(void)
 	
 	// Calculate the color of the particle
 	//Color0 = vec3(1.0, 0.5+0.5*sin(gTime), 1.0);
-	Color0 = vec3(1+sin(((sphere-PI)/2.0)+gTime), 0.0, 0.0);//0.5+0.5*sin(gTime), 1.0);
+	//Color0 = vec3(1+sin(((sphere-PI)/2.0)+gTime), 0.0, 0.0);//0.5+0.5*sin(gTime), 1.0);
+	Color0 = vec3(0.5+0.5*sin(sphere+gTime), 0.5+0.5*cos(sphere+gTime), 0.0);//0.5+0.5*sin(gTime), 1.0);
 
 	// Calculate the new position of the particle
-	vec3 new_position = vec3(0.5 * sin(sphere), 1.0 * sin(gTime), 0.5 * cos(sphere));
+	vec3 new_position = 4*vec3(0.5 * sin(sphere), 0, 0.5 * cos(sphere));
 
 
 	gl_Position = gModel * vec4(new_position, 1.0);
@@ -84,6 +85,7 @@ void main()
 		EndPrimitive();
 	}
 }
+
 
 #type fragment
 #version 440 core
