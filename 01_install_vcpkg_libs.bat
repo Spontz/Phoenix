384742158d39a24a690de02b8e00c5dbf97d35b7
Isaac Lascasas <isaaclascasas@gmail.com>
2020-11-01 22:29:09 +0100

    Workaround for broken vcpkg master.

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -2,7 +2,7 @@ rmdir vcpkg /s /q
 REM git clone https://github.com/Spontz/vcpkg.git
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-git checkout 83f88c7a629a8c681fa0f68c207377a9146dd7e5
+git checkout c34c4189ebbf0795e1f8e2de562c527a854c8db4
 call bootstrap-vcpkg.bat
 vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui glfw3 exprtk --triplet x64-windows-static
 cd..
