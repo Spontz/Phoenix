c9d46006fcef5b65827fa3d8bcb3497813c681be
xphere <xpherezn@gmail.com>
2020-12-19 11:13:20 +0100

    Update libs to Dec 18th

--- Phoenix/01_install_vcpkg_libs.bat
+++ Phoenix/01_install_vcpkg_libs.bat
@@ -2,7 +2,8 @@ rmdir vcpkg /s /q
 REM git clone https://github.com/Spontz/vcpkg.git
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-git checkout c34c4189ebbf0795e1f8e2de562c527a854c8db4
+rem checkout from Dec18th
+git checkout 66bac12e4e54cc858bdf7f93c5786010b6812b13
 call bootstrap-vcpkg.bat
-vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui glfw3 exprtk --triplet x64-windows-static
+vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui[docking-experimental] glfw3 exprtk --triplet x64-windows-static
 cd..
--- Phoenix/01_update_vcpkg_libs.bat
+++ Phoenix/01_update_vcpkg_libs.bat
@@ -1,7 +1,7 @@
 cd vcpkg
 git pull
 .\bootstrap-vcpkg.bat
-.\vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui glfw3 exprtk --triplet x64-windows-static
+.\vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui[docking-experimental] glfw3 exprtk --triplet x64-windows-static
 .\vcpkg upgrade --no-dry-run
 cd..
 pause
