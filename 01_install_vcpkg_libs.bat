27c6a3bdf40506d5552ad95061c6da91e49d38c4
Isaac Lascasas <IsaacLascasas@gmail.com>
2021-08-07 21:14:48 +0200

    Checking out 2021.05.12 vcpkg tag.

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -1,6 +1,7 @@
 rmdir vcpkg /s /q
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
+git checkout tags/2021.05.12
 powershell "dir *.ps* -recurse | Set-AuthenticodeSignature -Certificate (dir cert:\currentuser\my -code)"
 call bootstrap-vcpkg.bat
 vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui[opengl3-glad-binding,opengl3-gl3w-binding] glfw3 exprtk --triplet x64-windows
