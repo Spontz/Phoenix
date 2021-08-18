9faf54e0d092e6817027f3e29fc8dba88aac4f47
Isaac Lascasas <IsaacLascasas@gmail.com>
2021-08-19 01:27:46 +0200

    Renamed some pars from Logger.

--- Engine/src/core/utils/Logger.h
+++ Engine/src/core/utils/Logger.h
@@ -29,24 +29,24 @@ namespace Phoenix {
 
 	public:
 		template <class... _Types>
-		static void info(LogLevel level, const std::string_view fmt, const _Types&... args) {
-			info(level, vformat(fmt, std::make_format_args(args...)));
+		static void info(LogLevel level, const std::string_view msg, const _Types&... args) {
+			info(level, vformat(msg, std::make_format_args(args...)));
 		}
 
 		template <class... _Types>
-		static void sendEditor(const std::string_view fmt, const _Types&... args) {
-			sendEditor(vformat(fmt, std::make_format_args(args...)));
+		static void sendEditor(const std::string_view msg, const _Types&... args) {
+			sendEditor(vformat(msg, std::make_format_args(args...)));
 		}
 
 		template <class... _Types>
-		static void error(const std::string_view fmt, const _Types&... args) {
-			error(vformat(fmt, std::make_format_args(args...)));
+		static void error(const std::string_view msg, const _Types&... args) {
+			error(vformat(msg, std::make_format_args(args...)));
 		}
 
 	public:
-		static void info(LogLevel level, const std::string_view Message);
-		static void sendEditor(const std::string_view Message);
-		static void error(const std::string_view Message);
+		static void info(LogLevel level, const std::string_view msg);
+		static void sendEditor(const std::string_view msg);
+		static void error(const std::string_view msg);
 		static void setLogLevel(LogLevel level);
 		static void openLogFile();
 		static void closeLogFile();
