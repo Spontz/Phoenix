0fcd9c04290e9d74ee018d3e066bc2cee8d0ac14
xphere <xpherezn@gmail.com>
2022-10-12 21:59:23 +0200

    v.2.1.2 - Updated vcpkg libs

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -1,8 +1,7 @@
 rmdir vcpkg /s /q
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-rem git checkout tags/2022.03.10
-git checkout tags/2022.08.15
+git checkout tags/2022.09.27
 powershell "dir *.ps* -recurse | Set-AuthenticodeSignature -Certificate (dir cert:\currentuser\my -code)"
 call bootstrap-vcpkg.bat
 vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui[opengl3-binding,glfw-binding] exprtk --triplet x64-windows
