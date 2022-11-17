60870126572b500a63428f30a05e023007b68ac7
xphere <xpherezn@gmail.com>
2022-11-17 14:35:49 +0100

    Removed VS2019 compatibility

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -1,7 +1,7 @@
 rmdir vcpkg /s /q
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-git checkout tags/2022.09.27
+rem git checkout tags/2022.11.14
 powershell "dir *.ps* -recurse | Set-AuthenticodeSignature -Certificate (dir cert:\currentuser\my -code)"
 call bootstrap-vcpkg.bat
 vcpkg install --recurse freetype assimp ffmpeg[opencl,nonfree,nvcodec,x264,gpl,version3] stb glm glad imgui[opengl3-binding,glfw-binding] exprtk --triplet x64-windows
--- 03_run_cmake.bat
+++ 03_run_cmake.bat
@@ -1,11 +1,5 @@
 set project=phoenix
 
-rmdir %project%_vs2019 /s /q
-mkdir %project%_vs2019
-cd %project%_vs2019
-cmake.exe .. -DBUILD_SHARED_LIBS=ON -DVCPKG_TARGET_TRIPLET=x64-windows -G "Visual Studio 16 2019"
-cd..
-
 rmdir %project%_vs2022 /s /q
 mkdir %project%_vs2022
 cd %project%_vs2022
