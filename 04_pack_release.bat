105fe7e63ec11d95cdbe7c981abbfd1b0d8ce5f9
xphere <xpherezn@gmail.com>
2022-11-17 15:14:26 +0100

    Removed VS2019 references

--- 04_pack_release.bat
+++ 04_pack_release.bat
@@ -1,3 +1,2 @@
-tools\packer\upx.exe -9 -o phoenix_vs2019\Launcher\Release\Phoenix_packed.exe phoenix_vs2019\Launcher\Release\Phoenix.exe
 tools\packer\upx.exe -9 -o phoenix_vs2022\Launcher\Release\Phoenix_packed.exe phoenix_vs2022\Launcher\Release\Phoenix.exe
 pause
