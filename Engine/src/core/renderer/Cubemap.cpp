7de03062296e5c550eea10e515591d35a960497b
xphere <xpherezn@gmail.com>
2023-07-23 17:48:16 +0200

    Cubemap DSA

--- Engine/src/core/renderer/Cubemap.cpp
+++ Engine/src/core/renderer/Cubemap.cpp
@@ -17,6 +17,7 @@ namespace Phoenix {
 	{
 		if (m_cubemapID != 0) {
 			glDeleteTextures(1, &m_cubemapID);
+			m_filename.clear();
 			m_width.clear();
 			m_height.clear();
 			m_cubemapID = 0;
@@ -28,7 +29,10 @@ namespace Phoenix {
 	{
 		// If we already have loaded this cubemap, we unload it first
 		if (m_cubemapID > 0) {
-			glGenTextures(1, &m_cubemapID);
+			glDeleteTextures(1, &m_cubemapID);
+			m_filename.clear();
+			m_width.clear();
+			m_height.clear();
 			m_cubemapID = 0;
 			m_mem = 0;
 		}
@@ -37,60 +41,76 @@ namespace Phoenix {
 
 		stbi_set_flip_vertically_on_load(flip); // required for loading textures properly
 
-		glGenTextures(1, &m_cubemapID);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, m_cubemapID);
-
+		glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &m_cubemapID);
+		
 		int Width, Height, components;
-		for (unsigned int i = 0; i < faces_file_name.size(); i++)
-		{
-			m_filename.push_back(faces_file_name[i]);
-			unsigned char* data = stbi_load(faces_file_name[i].c_str(), &Width, &Height, &components, 0);
-			m_width.push_back(Width);
-			m_height.push_back(Height);
-			if (data)
-			{
-				GLenum internalFormat = 0;
-				GLenum dataFormat = 0;
-				if (components == 1) {
-					internalFormat = GL_R8;
-					dataFormat = GL_RED;
-				}
-				else if (components == 2) {
-					internalFormat = GL_RG8;
-					dataFormat = GL_RG;
-				}
-				else if (components == 3) {
-					internalFormat = GL_RGB8;
-					dataFormat = GL_RGB;
-				}
-				else if (components == 4) {
-					internalFormat = GL_RGBA8;
-					dataFormat = GL_RGBA;
-				}
-				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internalFormat, Width, Height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
-			}
+		GLuint internalFormat = 0;
+		GLuint dataFormat = 0;
+		stbi_uc *data;
 
-			else {
-				Logger::error("Failed loading cubemap from file: {}", faces_file_name[i]);
-				is_loaded = false;
+		// Use the first texture to get Width, Height, components, internalFormat and dataFormat
+		// TODO1: remove this to another function
+		// TODO2: Refactorizar el if por switch como en: https://github.com/BaronVerde/orf_n/blob/master/src/renderer/TextureCube.cpp
+		// TODO3: Aplicarlo despues a Texture.cpp
+		data = stbi_load(faces_file_name[0].c_str(), &Width, &Height, &components, 0);
+		if (data) {
+			if (components == 1) {
+				internalFormat = GL_R8;
+				dataFormat = GL_RED;
 			}
-			m_mem += (float)(Width * Height * 3) / 1048576.0f;		// Calculate the texture mem (in mb)
+			else if (components == 2) {
+				internalFormat = GL_RG8;
+				dataFormat = GL_RG;
+			}
+			else if (components == 3) {
+				internalFormat = GL_RGB8;
+				dataFormat = GL_RGB;
+			}
+			else if (components == 4) {
+				internalFormat = GL_RGBA8;
+				dataFormat = GL_RGBA;
+			}
 			stbi_image_free(data);
 		}
-		// Check if the cubemap images sizes are OK
-		for (unsigned int i = 0; i < faces_file_name.size(); i++)
-		{
-			if ((m_width[0] != m_width[i]) || (m_height[0] != m_height[i])) {
-				Logger::error("The cubemap image {} has a different size from the first one. Please check that all cubemap images have same size", faces_file_name[i]);
+		else {
+			Logger::error("Failed loading cubemap from file: {}", faces_file_name[0]);
+			is_loaded = false;
+		}
+
+		glTextureStorage2D(m_cubemapID, 1, internalFormat, Width, Height);
+
+		if (DEMO_checkGLError())
+			int kk = 0;
+		
+		for (unsigned int face = 0; face < faces_file_name.size(); ++face) {
+			data = stbi_load(faces_file_name[face].c_str(), &Width, &Height, &components, 0);
+			if (data) {
+				m_filename.push_back(faces_file_name[face]);
+				m_width.push_back(Width);
+				m_height.push_back(Height);
+				//glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + face, 0, internalFormat, Width, Height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
+				glTextureSubImage3D(m_cubemapID, 0, 0, 0,		// target, level , xOffset, yOffset
+					face, Width, Height, 1, dataFormat, 		// cube map face, width, height, 1 face a time, format
+					GL_UNSIGNED_BYTE, data);					// datatype, data pointer
+				if (DEMO_checkGLError()) {
+					Logger::error("Error uploading cubemap: {}, cubemap face: {}", faces_file_name[face], face);
+					Logger::error("cubemapID:{}, face:{}, width:{}, height:{}, dataFormat:{}", m_cubemapID,face,Width,Height,dataFormat);
+				}
+					m_mem += (float)(Width * Height * components) / 1048576.0f;		// Calculate the texture mem (in mb)
+				stbi_image_free(data);
+				data = NULL;
+			} else {
+				Logger::error("Failed loading cubemap from file: {}", faces_file_name[face]);
 				is_loaded = false;
 			}
 		}
 
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		glTextureParameteri(m_cubemapID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_cubemapID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTextureParameteri(m_cubemapID, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTextureParameteri(m_cubemapID, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTextureParameteri(m_cubemapID, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
 
 		stbi_set_flip_vertically_on_load(false); // Set always to "false"
 		return is_loaded;
