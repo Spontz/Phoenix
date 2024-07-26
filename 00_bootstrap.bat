ee7ee964f1f805a47a602f47f0033269f0558d86
perefm <perefm@gmail.com>
2024-07-26 14:36:52 +0200

    Removed freetype lib from Cmake

--- 00_bootstrap.bat
+++ 00_bootstrap.bat
@@ -1,4 +1,3 @@
 call 01_install_vcpkg_libs.bat
-powershell "'.\02_install_third_party_libs.ps1' | Set-AuthenticodeSignature -Certificate (dir cert:\currentuser\my -code)"
 call 02_run_cmake.bat
 pause
