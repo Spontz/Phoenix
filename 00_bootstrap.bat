a542036c78fde1a944c643d1c8efa592fe5645b3
perefm <perefm@gmail.com>
2024-07-26 13:37:00 +0200

    Removed downloading of bass library

--- 00_bootstrap.bat
+++ 00_bootstrap.bat
@@ -1,5 +1,4 @@
 call 01_install_vcpkg_libs.bat
 powershell "'.\02_install_third_party_libs.ps1' | Set-AuthenticodeSignature -Certificate (dir cert:\currentuser\my -code)"
-Powershell.exe -executionpolicy remotesigned -File .\02_install_third_party_libs.ps1
-call 03_run_cmake.bat
+call 02_run_cmake.bat
 pause
