575ae0bc34d395e53d46a01e7b1fddc61cd6c88f
perefm <perefm@gmail.com>
2024-08-14 17:20:25 +0200

    Update on resources shaders

--- Launcher/data/resources/shaders/sections/drawParticlesFbo/drawParticlesFbo.glsl
+++ Launcher/data/resources/shaders/sections/drawParticlesFbo/drawParticlesFbo.glsl
@@ -4,7 +4,7 @@ layout (location = 0) in int Type;
 layout (location = 1) in int ID;
 layout (location = 2) in vec3 InitPosition;
 layout (location = 3) in vec3 Randomness;
-layout (location = 4) in vec4 InitColor;
+layout (location = 4) in vec4 InitColor; // Hack, here we store in .xy the texture coordinates of the FBO
 layout (location = 5) in float Life;
 
 uniform mat4 m4ViewModel; // view x Model matrix
@@ -14,7 +14,7 @@ out VS_OUT
 {
 	flat int 	Type;
 	flat int 	ID;
-	vec4		Color;
+	vec2		FboTexCoord;
 	vec4		Position;
 } vs_out;
 
@@ -22,7 +22,7 @@ void main(void)
 {
 	vs_out.Type = Type;
 	vs_out.ID = ID;
-	vs_out.Color = InitColor;
+	vs_out.FboTexCoord = InitColor.xy;
 	vs_out.Position = m4ViewModel * vec4(InitPosition, 1.0);
 }
 
@@ -42,7 +42,7 @@ in VS_OUT
 {
 	flat int 	Type;
 	flat int	ID;
-	vec4		Color;
+	vec2		FboTexCoord;
 	vec4		Position;
 } gs_in[];
 
@@ -50,7 +50,7 @@ in VS_OUT
 out GS_OUT
 {
 	flat int	ID;
-	vec4		Color;
+	vec2		FboTexCoord;
 	vec2		TexCoord;
 } gs_out;
 
@@ -60,7 +60,7 @@ out GS_OUT
 void main()
 {
 	gs_out.ID = gs_in[0].ID;
-	gs_out.Color = gs_in[0].Color;
+	gs_out.FboTexCoord = gs_in[0].FboTexCoord;
 
 	if (gs_in[0].Type == PARTICLE_TYPE_EMITTER) {
 	
@@ -98,17 +98,19 @@ void main()
 #version 440 core
 layout (location = 0) out vec4 FragColor;
 
+uniform sampler2D screenTexture;
 uniform sampler2D partTexture;
 
 in GS_OUT
 {
 	flat int	ID;
-	vec4		Color;
+	vec2		FboTexCoord;
 	vec2		TexCoord;
 } fs_in;
 
 
 void main(void)
 {
-	FragColor = texture(partTexture, fs_in.TexCoord) * fs_in.Color;
+	vec4 partColor = texture(screenTexture, fs_in.FboTexCoord);
+	FragColor = texture(partTexture, fs_in.TexCoord) * partColor;
 }
