a74cd7809e69976e6fbd1660c0f9650e685a2b14
xphere <xpherezn@gmail.com>
2025-11-20 22:56:58 +0100

    upgrade to VS 2026

--- 02_run_cmake.bat
+++ 02_run_cmake.bat
@@ -1,9 +1,9 @@
 set project=phoenix
 
-rmdir %project%_vs2022 /s /q
-mkdir %project%_vs2022
-cd %project%_vs2022
-cmake.exe .. -DBUILD_SHARED_LIBS=ON -DVCPKG_TARGET_TRIPLET=x64-windows -G "Visual Studio 17 2022"
+rmdir %project%_vs2026 /s /q
+mkdir %project%_vs2026
+cd %project%_vs2026
+cmake.exe .. -DBUILD_SHARED_LIBS=ON -DVCPKG_TARGET_TRIPLET=x64-windows -G "Visual Studio 18 2026"
 cd..
 
 pause
